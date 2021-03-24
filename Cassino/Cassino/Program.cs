42f00a47ceb4a7debc6527c3d1fd14410ce4af98
Samuel Santos Barreto Pereira <samucateclas@gmail.com>
2021-03-23 23:11:56 -0300

    Finish player2

--- Cassino/Cassino/Program.cs
+++ Cassino/Cassino/Program.cs
@@ -13,11 +13,10 @@ namespace Cassino
             Guy player = new Guy() { Cash = 100, Name = "Player 1" };
             Guy player2 = new Guy() { Cash = 100, Name = "Player 2" };
 
-            Console.WriteLine("Welcome to the Cassino. The odds are " + odds.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Welcome to the Cassino " + player.Name + " .The odds are " + odds.ToString("F2", CultureInfo.InvariantCulture));
             while (player.Cash > 0)
             {
                 player.WriteMyInfo();
-                player2.WriteMyInfo();
                 Console.Write("How much money do you want to bet: ");
                 string howMuch = Console.ReadLine();
                 if (int.TryParse(howMuch, out int amount))
@@ -43,6 +42,37 @@ namespace Cassino
                 }
             }
             Console.WriteLine("The house always wins!");
+
+            Console.WriteLine("Welcome to the Cassino " + player2.Name + " .The odds are " + odds.ToString("F2", CultureInfo.InvariantCulture));
+            while (player2.Cash > 0)
+            {
+                player2.WriteMyInfo();
+                Console.Write("How much money do you want to bet: ");
+                string howMuch = Console.ReadLine();
+                if (int.TryParse(howMuch, out int amount))
+                {
+                    int pot = player2.GiveCash(amount) * 2;
+                    if (pot > 0)
+                    {
+                        if (random.NextDouble() > odds)
+                        {
+                            int winnings = pot;
+                            Console.WriteLine("You win " + winnings);
+                            player2.ReceiveCash(winnings);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Bad luck, you lose.");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid number.");
+                }
+            }
+            Console.WriteLine("The house always wins!");
         }
+
     }
 }
